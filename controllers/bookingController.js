ad33aceb2891f7ab531f63a469e8c374050f2079
Binita Shrestha <KAN074BCT018@student.kec.edu.np>
2020-12-03 23:13:31 +0545

    fixes

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -54,10 +54,14 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 // });
 
 const createBookingCheckout = async (session) => {
-  const tour = session.client_reference_id;
-  const user = await User.findOne({ email: session.customer_email });
-  const price = session.display_items[0].amount / 100;
-  await Booking.create({ tour, user, price });
+  try {
+    const tour = session.client_reference_id;
+    const user = await User.findOne({ email: session.customer_email });
+    const price = session.amount_total / 100;
+    await Booking.create({ tour, user, price });
+  } catch (err) {
+    console.log(err.message);
+  }
 };
 
 exports.webhookCheckout = (req, res, next) => {
